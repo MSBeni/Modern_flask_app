06bf248efb90220d9b80a1c1a6770ffa541bb239
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-01-17 19:23:42 -0500

    Starting with new store design

--- app/app.py
+++ app/app.py
@@ -3,9 +3,34 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World"
+# Create a store
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# Get the name of all stores
+@app.route('/store', methods=['GET'])
+def get_all_stores():
+    pass
+
+
+# Get the name of a specific store
+@app.route('/store/<string:name>', methods=['GET'])   # 127.0.0.1:5000/store/store_name
+def get_store(name):
+    pass
+
+
+# Get the name of a specific store
+@app.route('/store/<string:name>/item', methods=['POST'])   # 127.0.0.1:5000/store/store_name/item
+def create_item_in_store(name):
+    pass
+
+
+# Get all the items in a specific store
+@app.route('/store/<string:name>/item', methods=['GET'])   # 127.0.0.1:5000/store/store_name/item
+def get_all_items_in_store(name):
+    pass
 
 
 if __name__ == '__main__':
