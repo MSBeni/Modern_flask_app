4580e68fd83cdb3b0a10138e41e21285e38af231
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-01-18 11:53:13 -0500

    added initial simple restful api 3

--- simple_RESTFUL_app/code/app.py
+++ simple_RESTFUL_app/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -6,25 +6,31 @@ api = Api(app)
 
 items = []
 
+items_2 = [{'name': 'book', 'price': 12}, {'name': 'piano', 'price': 20}]
+x = filter(lambda x: x['name'] == 'book', items_2)
+print(*x)
+
+
+class Items(Resource):
+    def get(self):
+        return {'items': items}
+
+
 class Item(Resource):
-    # def get(self):
-    #     return {'items': items}
-
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return {'item': item}
-
-        return {'message': 'No such a item available ...'}, 404
-
+        item = next(filter(lambda x:x['name'] == name, items), None)
+        return {'item': item}, 200 if item is not None else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12}
+        item_price = request.get_json(silent=True)
+
+        item = {'name': name, 'price': item_price['price']}
         items.append(item)
         return {'Added item': item}, 201
 
 
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', port=5000)
+    app.run('127.0.0.1', port=5000, debug=True)
