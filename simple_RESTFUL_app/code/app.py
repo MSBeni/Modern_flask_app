68b99df68a76d939bde25558df6b70a20af92042
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-01-18 11:30:23 -0500

    added initial simple restful api 2

--- simple_RESTFUL_app/code/app.py
+++ simple_RESTFUL_app/code/app.py
@@ -4,13 +4,27 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
+class Item(Resource):
+    # def get(self):
+    #     return {'items': items}
+
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return {'item': item}
+
+        return {'message': 'No such a item available ...'}, 404
 
 
-api.add_resource(Student, '/student/<string:name>')
+    def post(self, name):
+        item = {'name': name, 'price': 12}
+        items.append(item)
+        return {'Added item': item}, 201
+
+
+api.add_resource(Item, '/item/<string:name>')
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port=5000)
