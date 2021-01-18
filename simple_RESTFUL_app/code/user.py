6d645eea15df26a1a9f9cb451ed22a36124af66f
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-01-18 18:53:16 -0500

    added sqlite user fetching to User class

--- simple_RESTFUL_app/code/user.py
+++ simple_RESTFUL_app/code/user.py
@@ -1,5 +1,5 @@
 class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
+    def __init__(self, id_, username, password):
+        self.id = id_
         self.username = username
         self.password = password
