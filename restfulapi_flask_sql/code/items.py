0a444e49be65900ae8bf36d750a6d5b73d69458e
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-01-19 16:41:58 -0500

    Added the item class code 5

--- restfulapi_flask_sql/code/items.py
+++ restfulapi_flask_sql/code/items.py
@@ -5,7 +5,12 @@ from flask_restful import Resource, reqparse
 
 class Items(Resource):
     def get(self):
-        return {'items': items}
+        connection = sqlite3.connect('data.db')
+        cur = connection.cursor()
+        data = cur.execute("SELECT * FROM items")
+        row = data.fetchall()
+        connection.close()
+        return {'items': row}
 
 
 class Item(Resource):
@@ -15,45 +20,61 @@ class Item(Resource):
                         required=True,
                         help='This field cannot be empty')
 
-    @jwt_required()
-    def get(self, name):
+    @classmethod
+    def get_item_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cur = connection.cursor()
         data = cur.execute("SELECT * FROM items WHERE items.name=?", (name,))
         row = data.fetchone()
         connection.close()
         if row:
-            return {'itme': {"name": row['name'], "price": row['price']}}
+            return {'item': {"name": row[0], "price": row[1]}}
+
+    @jwt_required()
+    def get(self, name):
+        item = self.get_item_by_name(name)
+        if item:
+            return item
 
         return {"message": "item not found ..."}, 404
 
     def post(self, name):
-        if next(filter(lambda x:x['name'] == name, items), None):
+        if self.get_item_by_name(name):
             return "Item {} already exits".format(name), 400
         # parsing to the json payload with the parse args as defined class Item RequestParser
         item_price = Item.parser.parse_args()
-
+        connection = sqlite3.connect('data.db')
+        cur = connection.cursor()
+        cur.execute("INSERT INTO items VALUES (?,?)", (name, item_price['price']))
+        connection.commit()
+        connection.close()
         item = {'name': name, 'price': item_price['price']}
-        items.append(item)
+
         return {'Added item': item}, 201
 
     @jwt_required()
     def delete(self, name):
-        global items
-        if next(filter(lambda x: x['name'] == name, items), None) is None:
+        if self.get_item_by_name(name) is None:
             return "There is no item named {} to be deleted".format(name), 400
-        # item = next(filter(lambda x: x['name'] == name, items), None)
-        # items.remove(item)
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cur = connection.cursor()
+        cur.execute("DELETE FROM items WHERE items.name=?", (name,))
+        connection.commit()
+        connection.close()
         return {'message': "item deleted"}, 200
 
     def put(self, name):
         # parsing to the json payload with the parse args as defined class Item RequestParser
         req_price = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.get_item_by_name(name)
+        connection = sqlite3.connect('data.db')
+        cur = connection.cursor()
         if item is None:
-            item = {'name': name, 'price': req_price['price']}
-            items.append(item)
+            cur.execute("INSERT INTO items VALUES (?,?)", (name, req_price['price']))
+            return {"message": "Item Added"}, 201
         else:
-            item.update(req_price)
+            cur.execute("UPDATE items SET price=? WHERE items.name=?", (req_price['price'], name))
+
+        connection.commit()
+        connection.close()
         return item
